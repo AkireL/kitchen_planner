f06db82dc710a0dec1acb2f573232631e4806a00
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-01-22 00:06:21 -0600

    added mockdata and fix css

---
+++ kitchen_planner/src/__fixture__/MockRecipes.ts
@@ -0,0 +1,282 @@
+export const recipesList = [
+  {
+    day: "Lunes",
+    id: 1,
+    list: [
+      {
+        id: 1,
+        title: 'Medieval Strategic War Campaigns',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "a preparation",
+        duration: '10 minutes',
+      },
+      {
+        id: 2,
+        title: 'Underwater VR Exploration Adventure',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '11 minutes',
+      },
+      {
+        id: 3,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+      {
+        id: 22,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+      {
+        id: 23,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+      {
+        id: 24,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+      {
+        id: 26,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+      {
+        id: 27,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+    ],
+  },
+  {
+    day: "Martes",
+    id: 2,
+    list: [
+      {
+        id: 4,
+        title: 'Medieval Strategic War Campaigns',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '10 minutes',
+      },
+      {
+        id: 5,
+        title: 'Underwater VR Exploration Adventure',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '11 minutes',
+      },
+      {
+        id: 6,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        duration: '9 minutes',
+      },
+    ]
+  },
+  {
+    day: "Miércoles",
+    id: 3,
+    list: [
+      {
+        id: 6,
+        title: 'Medieval Strategic War Campaigns',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        advanced: true,
+        duration: '10 minutes',
+      },
+      {
+        id: 7,
+        title: 'Underwater VR Exploration Adventure',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        advanced: true,
+        duration: '11 minutes',
+      },
+      {
+        id: 8,
+        title: '1920s Mystery Detective Chronicles',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+        advanced: false,
+        duration: '9 minutes',
+      },
+    ],
+  },
+  {
+    day: "Jueves",
+    id: 4,
+    list: [
+      {
+        id: 9,
+        title: 'Medieval Strategic War Campaigns',
+        duration: '10 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 10,
+        title: 'Underwater VR Exploration Adventure',
+        duration: '11 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 11,
+        title: '1920s Mystery Detective Chronicles',
+        duration: '9 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+    ]
+  },
+  {
+    day: "Viernes",
+    id: 5,
+    list: [
+      {
+        id: 12,
+        title: 'Medieval Strategic War Campaigns',
+        duration: '10 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 13,
+        title: 'Underwater VR Exploration Adventure',
+        duration: '11 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 14,
+        title: '1920s Mystery Detective Chronicles',
+        duration: '9 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+    ],
+  },
+  {
+    day: "Sábado",
+    id: 6,
+    list: [
+      {
+        id: 15,
+        title: 'Medieval Strategic War Campaigns',
+        duration: '10 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 16,
+        title: 'Underwater VR Exploration Adventure',
+        duration: '11 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 17,
+        title: '1920s Mystery Detective Chronicles',
+        duration: '9 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+    ],
+  },
+  {
+    day: "Domingo",
+    id: 7,
+    list: [
+      {
+        id: 18,
+        title: 'Medieval Strategic War Campaigns',
+        duration: '10 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 19,
+        title: 'Underwater VR Exploration Adventure',
+        duration: '11 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+      {
+        id: 20,
+        title: '1920s Mystery Detective Chronicles',
+        duration: '9 minutes',
+        ingredients: [
+          "tomate", "chiles", "ajo"
+        ],
+        preparation: "",
+      },
+    ],
+  },
+];
+
