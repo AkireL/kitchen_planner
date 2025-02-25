095e2cb25d5861290bd471868b3820b3ee6fda22
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-02-25 17:56:13 -0600

    added variables to manage state

--- kitchen_planner/src/composables/userStore.ts
+++ kitchen_planner/src/composables/userStore.ts
@@ -3,14 +3,31 @@ import { ref } from 'vue';
 
 export const useUserStore = defineStore('user-session', () => {
   const token = ref<string>();
-  const isAuthenticated = ref<boolean>(true);
+  const isAuthenticated = ref<boolean>(false);
+  const user = ref(null);
 
   function getToken() {
     return token.value;
   }
 
+  function setToken(jwt: string) {
+    token.value = jwt;
+  }
+
+  function setUser(value) {
+    user.value = value;
+    isAuthenticated.value = true;
+  }
+
+  function getUser() {
+    return user.value;
+  }
+
   return {
     getToken,
+    setToken,
     isAuthenticated,
+    setUser,
+    getUser,
   };
 });
