1c879cb6019105b6fede1806a75c5ace06044c5e
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-02-27 12:18:30 -0600

    added rule

--- kitchen_planner/src/views/LogInView.vue
+++ kitchen_planner/src/views/LogInView.vue
@@ -15,42 +15,42 @@ import {
 import { useRouter } from 'vue-router';
 
 const loading = ref<boolean>(false);
-const userName = ref<string>("");
-const password = ref<string>("");
-const rules = [];
+const userName = ref<string>('');
+const password = ref<string>('');
 
 const store = useUserStore();
 const router = useRouter();
 
-function submit()
-{
+const rules = (value: string | number | Date) => !!value || 'Este campo es obligatorio';
+
+function submit() {
   const data = {
-    "password": password.value,
-    "username": userName.value,
-    "grant_type": "password",
-    "scope": "",
-    "client_id": "",
-    "client_secret": "",
-  }
+    password: password.value,
+    username: userName.value,
+    grant_type: 'password',
+    scope: '',
+    client_id: '',
+    client_secret: '',
+  };
 
   logIn(data)
-  .then(({data}) => {
+    .then(({ data }) => {
       store.setToken(data.access_token);
 
       getCurrentUser()
-      .then(({data})=> {
-        store.setUser(data);
-        router.push({ name: 'homeRecipe' });
-      })
-      .catch((e) => {
-        console.log("ERROR GET USER")
-        console.log(e);
-      })
-  })
-  .catch((e) => {
-    console.log("Error LogIn");
-    console.log(e);
-  });
+        .then(({ data }) => {
+          store.setUser(data);
+          router.push({ name: 'homeRecipe' });
+        })
+        .catch((e) => {
+          console.log('ERROR GET USER');
+          console.log(e);
+        });
+    })
+    .catch((e) => {
+      console.log('Error LogIn');
+      console.log(e);
+    });
 }
 </script>
 <template>
@@ -60,16 +60,25 @@ function submit()
         <v-form validate-on="submit lazy" @submit.prevent="submit">
           <v-sheet class="mx-auto" max-width="300">
             <v-card-title>LogIn</v-card-title>
-            <v-text-field v-model="userName" :rules="rules" label="User name"></v-text-field>
-            <v-text-field v-model="password" :rules="rules" label="Password"></v-text-field>
+            <v-text-field v-model="userName" :rules="[rules]" label="User name"></v-text-field>
+            <v-text-field v-model="password" :rules="[rules]" label="Password"></v-text-field>
 
-            <v-btn :loading="loading" class="mt-2 mb-5" color="primary" text="Submit" type="submit" block></v-btn>
+            <v-btn
+              :loading="loading"
+              class="mt-2 mb-5"
+              color="primary"
+              text="Submit"
+              type="submit"
+              block
+            ></v-btn>
           </v-sheet>
         </v-form>
         <v-divider></v-divider>
         <div class="ml-5 mt-5">
           New to Kitchen planner?
-          <v-btn @click="() => $router.push({ name: 'signIn' })" class="mb-5" variant="plain">Create an account</v-btn>
+          <v-btn @click="() => $router.push({ name: 'signIn' })" class="mb-5" variant="plain"
+            >Create an account</v-btn
+          >
         </div>
       </v-card>
     </v-container>
--- kitchen_planner/src/views/SignInView.vue
+++ kitchen_planner/src/views/SignInView.vue
@@ -15,21 +15,21 @@ import {
 import { useRouter } from 'vue-router';
 
 const loading = ref<boolean>(false);
-const userName = ref<string>("");
-const email = ref<string>("");
-const fullName = ref<string>("");
-const password = ref<string>("");
-const rules = [];
+const userName = ref<string>('');
+const email = ref<string>('');
+const fullName = ref<string>('');
+const password = ref<string>('');
+const rules = (value: string | number | Date) => !!value || 'Este campo es obligatorio';
 
 const store = useUserStore();
 const router = useRouter();
 
 function submit() {
   const data = {
-    "username": userName.value,
-    "password": password.value,
-    "email": email.value,
-    "full_name": fullName.value,
+    username: userName.value,
+    password: password.value,
+    email: email.value,
+    full_name: fullName.value,
   };
 
   signIn(data)
@@ -38,11 +38,10 @@ function submit() {
       store.setUser(data.data.user);
 
       router.push({ name: 'homeRecipe' });
-
     })
     .catch((e) => {
       console.log(e);
-    })
+    });
 }
 </script>
 <template>
@@ -52,19 +51,28 @@ function submit() {
         <v-form validate-on="submit lazy" @submit.prevent="submit">
           <v-sheet class="mx-auto" max-width="300">
             <v-card-title>Sign in</v-card-title>
-            <v-text-field v-model="userName" :rules="rules" label="User name"></v-text-field>
+            <v-text-field v-model="userName" :rules="[rules]" label="User name"></v-text-field>
 
-            <v-text-field v-model="fullName" :rules="rules" label="Your fullname"></v-text-field>
+            <v-text-field v-model="fullName" :rules="[rules]" label="Your fullname"></v-text-field>
 
-            <v-text-field v-model="email" :rules="rules" label="email"></v-text-field>
+            <v-text-field v-model="email" :rules="[rules]" label="email"></v-text-field>
 
-            <v-text-field v-model="password" :rules="rules" label="Password"></v-text-field>
+            <v-text-field v-model="password" :rules="[rules]" label="Password"></v-text-field>
 
-            <v-btn :loading="loading" class="mt-2 mb-5" color="primary" text="Submit" type="submit" block></v-btn>
+            <v-btn
+              :loading="loading"
+              class="mt-2 mb-5"
+              color="primary"
+              text="Submit"
+              type="submit"
+              block
+            ></v-btn>
           </v-sheet>
         </v-form>
 
-        <v-btn @click="() => $router.push({ name: 'logIn' })" class="mb-2 ml-5" variant="plain">Log In</v-btn>
+        <v-btn @click="() => $router.push({ name: 'logIn' })" class="mb-2 ml-5" variant="plain"
+          >Log In</v-btn
+        >
       </v-card>
     </v-container>
   </v-layout>
