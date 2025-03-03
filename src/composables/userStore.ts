05516fba4ad9d972f353d0f3f12c9b59b3bef428
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-03-03 13:57:01 -0600

    rename sign out

--- src/composables/userStore.ts
+++ src/composables/userStore.ts
@@ -24,7 +24,7 @@ export const useUserStore = defineStore('user-session', () => {
     return true;
   }
 
-  function singUp() {
+  function logOut() {
     token.value = null;
     user.value = null;
     localStorage.removeItem('kitchen');
@@ -55,6 +55,6 @@ export const useUserStore = defineStore('user-session', () => {
     isAuthenticated,
     setUser,
     getUser,
-    singUp,
+    logOut,
   };
 });
