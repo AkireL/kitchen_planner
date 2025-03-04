f3bb225bfe672bcd581e9a9614b3ad694e2a6ecd
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-03-03 21:28:26 -0600

    fix cs and catch KitchenAPIException

--- src/composables/recipeStore.ts
+++ src/composables/recipeStore.ts
@@ -1,99 +1,112 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 import type { Recipe, RecipeFiltersParams } from '@/types';
-import { createRecipesService, deleteRecipesService, retrieveRecipesService, updateRecipesService } from '@/services/kitchen_api/recipeService'
+import {
+  createRecipesService,
+  deleteRecipesService,
+  retrieveRecipesService,
+  updateRecipesService,
+} from '@/services/kitchen_api/recipeService';
 import { formatDate } from '@/helpers/helpers';
+import { useRouter } from 'vue-router';
+import { KitchenAPIException } from '@/services/kitchen_api/kitchen_api_exception';
 
 export const useRecipeStore = defineStore('search-filters', () => {
-  const recipesList = ref<Recipe[]>([])
+  const recipesList = ref<Recipe[]>([]);
 
-  const retrieveRecipes = (startDate:Date | null = null, endDate:Date | null = null) => {
+  const router = useRouter();
+
+  const retrieveRecipes = (startDate: Date | null = null, endDate: Date | null = null) => {
     const filters: RecipeFiltersParams = {
-      "start_date": "",
-      "end_date": ""
+      start_date: '',
+      end_date: '',
     };
 
     if (startDate != null && endDate != null) {
-        filters['start_date'] = formatDate(startDate);
-        filters['end_date'] = formatDate(endDate);
+      filters['start_date'] = formatDate(startDate);
+      filters['end_date'] = formatDate(endDate);
     }
 
     retrieveRecipesService(filters)
-      .then(function ({data}) {
-        recipesList.value= data.data;
+      .then(function ({ data }) {
+        recipesList.value = data.data;
       })
-      .catch(() => {
-        recipesList.value= [];
+      .catch((error) => {
+        recipesList.value = [];
+
+        if (error instanceof KitchenAPIException && error.code === 401) {
+          router.push({ name: 'logIn' });
+        }
       });
-  }
+  };
 
   const getById = (id: number | string) => {
     const recipeTmp = recipesList.value.filter((item) => {
-      return item.id == id
-    })
+      return item.id == id;
+    });
 
     if (recipeTmp) {
       if (recipeTmp[0].schedule_at) {
-        const [year, month, day] = recipeTmp[0].schedule_at.split('-')
-        const formattedDate = `${year}-${month}-${day}`
-        recipeTmp[0].schedule_at = formattedDate
-        return recipeTmp[0]
+        const [year, month, day] = recipeTmp[0].schedule_at.split('-');
+        const formattedDate = `${year}-${month}-${day}`;
+        recipeTmp[0].schedule_at = formattedDate;
+        return recipeTmp[0];
       }
 
-      const date = new Date().toISOString().slice(0, 10)
-      const [year, month, day] = date.split('-')
-      const formattedDate = `${year}-${month}-${day}`
-      recipeTmp[0].schedule_at = formattedDate
+      const date = new Date().toISOString().slice(0, 10);
+      const [year, month, day] = date.split('-');
+      const formattedDate = `${year}-${month}-${day}`;
+      recipeTmp[0].schedule_at = formattedDate;
 
-      return recipeTmp[0]
+      return recipeTmp[0];
     }
-  }
+  };
 
   const updateRecipe = (data: Recipe) => {
     const index = recipesList.value.findIndex((item) => {
       return item.id == data.id;
     });
 
-    if (index> -1) {
+    if (index > -1) {
       updateRecipesService(data.id, data)
-      .then(function(){
+        .then(function () {
           recipesList.value[index] = data;
         })
-        .catch(function(error){
+        .catch(function (error) {
           console.log(error);
         });
     }
-  }
+  };
 
   const removeRecipe = (id: number | string) => {
     recipesList.value = recipesList.value.filter((item) => {
-      return item.id != id
+      return item.id != id;
     });
 
     deleteRecipesService(id)
-      .then(function(data){
-        console.log("DELETE", data);
+      .then(function (data) {
+        console.log('DELETE', data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
 
-  const addRecipe =  (newItem: Recipe) => {
-      const item = {...newItem};
+  const addRecipe = (newItem: Recipe) => {
+    const item = { ...newItem };
 
-      if(! newItem.preparation || newItem.preparation.length <=0) {
-        delete item["preparation"];
-      }
+    if (!newItem.preparation || newItem.preparation.length <= 0) {
+      delete item['preparation'];
+    }
 
-      createRecipesService(item)
-        .then(function({data}) {
-          recipesList.value.push(data);
-        })
-        .catch(function (error){
-          console.log(error);
-        });
-  }
+    createRecipesService(item)
+      .then(function ({ data }) {
+        recipesList.value.push(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return {
     recipesList,
@@ -102,5 +115,5 @@ export const useRecipeStore = defineStore('search-filters', () => {
     addRecipe,
     updateRecipe,
     removeRecipe,
-  }
-})
+  };
+});
