3d145a971e852175d62f5c930f5f478b45d71daf
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2026-05-20 17:52:27 -0600

    fix: actualizar tipo de recetas en MockRecipes eliminando user_id

--- src/__fixture__/MockRecipes.ts
+++ src/__fixture__/MockRecipes.ts
@@ -1,266 +1,214 @@
 import type { Recipe } from '@/types';
 
-export const recipesList: Recipe[] =
-  [
-      {
-        id: 1,
-        title: 'Medieval Strategic War Campaigns',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "a preparation",
-        duration: '10 minutes',
-        schedule_at: "2025-01-20",
-      },
-      {
-        id: 2,
-        title: 'two recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        duration: '11 minutes',
-        schedule_at: "2025-01-20",
-      },
-      {
-        id: 3,
-        title: 'three recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        duration: '9 minutes',
-        schedule_at: "2025-01-20",
-      },
-      {
-        id: 22,
-        title: 'four recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-20",
-        duration: '9 minutes',
-      },
-      {
-        id: 23,
-        title: 'five recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-20",
-        duration: '9 minutes',
-      },
-      {
-        id: 24,
-        title: 'six recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-20",
-        duration: '9 minutes',
-      },
-      {
-        id: 26,
-        title: 'six recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-20",
-        duration: '9 minutes',
-      },
-      {
-        id: 27,
-        title: 'seven recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-20",
-        duration: '9 minutes',
-      },
-      {
-        id: 4,
-        title: 'eight recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-21",
-        duration: '10 minutes',
-      },
-      {
-        id: 5,
-        title: 'nine recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-21",
-        duration: '11 minutes',
-      },
-      {
-        id: 101,
-        title: 'ten recipe',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-21",
-        duration: '9 minutes',
-      },
-      {
-        id: 100,
-        title: 'Medieval Strategic War Campaigns',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-22",
-        duration: '10 minutes',
-      },
-      {
-        id: 7,
-        title: 'Underwater VR Exploration Adventure',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-22",
-        duration: '11 minutes',
-      },
-      {
-        id: 8,
-        title: '1920s Mystery Detective Chronicles',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        preparation: "",
-        schedule_at: "2025-01-22",
-        duration: '9 minutes',
-      },
-      {
-        id: 9,
-        title: 'Medieval Strategic War Campaigns',
-        duration: '10 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-23",
-        preparation: "",
-      },
-      {
-        id: 10,
-        title: 'Underwater VR Exploration Adventure',
-        duration: '11 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-23",
-        preparation: "",
-      },
-      {
-        id: 11,
-        title: '1920s Mystery Detective Chronicles',
-        duration: '9 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-23",
-        preparation: "",
-      },
-      {
-        id: 12,
-        title: 'Medieval Strategic War Campaigns',
-        duration: '10 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-24",
-        preparation: "",
-      },
-      {
-        id: 13,
-        title: 'Underwater VR Exploration Adventure',
-        duration: '11 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-24",
-        preparation: "",
-      },
-      {
-        id: 14,
-        title: '1920s Mystery Detective Chronicles',
-        duration: '9 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-24",
-        preparation: "",
-      },
-      {
-        id: 15,
-        title: 'Medieval Strategic War Campaigns',
-        duration: '10 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-25",
-        preparation: "",
-      },
-      {
-        id: 16,
-        title: 'Underwater VR Exploration Adventure',
-        duration: '11 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-25",
-        preparation: "",
-      },
-      {
-        id: 17,
-        title: '1920s Mystery Detective Chronicles',
-        duration: '9 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-25",
-        preparation: "",
-      },
-      {
-        id: 18,
-        title: 'Medieval Strategic War Campaigns',
-        duration: '10 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-26",
-        preparation: "",
-      },
-      {
-        id: 19,
-        title: 'Underwater VR Exploration Adventure',
-        duration: '11 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-26",
-        preparation: "",
-      },
-      {
-        id: 20,
-        title: '1920s Mystery Detective Chronicles',
-        duration: '9 minutes',
-        ingredients: [
-          "tomate", "chiles", "ajo"
-        ],
-        schedule_at: "2025-01-26",
-        preparation: "",
-      },
-];
+type MockRecipe = Omit<Recipe, 'user_id'>;
 
+export const recipesList: MockRecipe[] = [
+  {
+    id: 1,
+    title: 'Medieval Strategic War Campaigns',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: 'a preparation',
+    duration: '10 minutes',
+    schedule_at: '2025-01-20',
+  },
+  {
+    id: 2,
+    title: 'two recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    duration: '11 minutes',
+    schedule_at: '2025-01-20',
+  },
+  {
+    id: 3,
+    title: 'three recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    duration: '9 minutes',
+    schedule_at: '2025-01-20',
+  },
+  {
+    id: 22,
+    title: 'four recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-20',
+    duration: '9 minutes',
+  },
+  {
+    id: 23,
+    title: 'five recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-20',
+    duration: '9 minutes',
+  },
+  {
+    id: 24,
+    title: 'six recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-20',
+    duration: '9 minutes',
+  },
+  {
+    id: 26,
+    title: 'six recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-20',
+    duration: '9 minutes',
+  },
+  {
+    id: 27,
+    title: 'seven recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-20',
+    duration: '9 minutes',
+  },
+  {
+    id: 4,
+    title: 'eight recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-21',
+    duration: '10 minutes',
+  },
+  {
+    id: 5,
+    title: 'nine recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-21',
+    duration: '11 minutes',
+  },
+  {
+    id: 101,
+    title: 'ten recipe',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-21',
+    duration: '9 minutes',
+  },
+  {
+    id: 100,
+    title: 'Medieval Strategic War Campaigns',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-22',
+    duration: '10 minutes',
+  },
+  {
+    id: 7,
+    title: 'Underwater VR Exploration Adventure',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-22',
+    duration: '11 minutes',
+  },
+  {
+    id: 8,
+    title: '1920s Mystery Detective Chronicles',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    preparation: '',
+    schedule_at: '2025-01-22',
+    duration: '9 minutes',
+  },
+  {
+    id: 9,
+    title: 'Medieval Strategic War Campaigns',
+    duration: '10 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-23',
+    preparation: '',
+  },
+  {
+    id: 10,
+    title: 'Underwater VR Exploration Adventure',
+    duration: '11 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-23',
+    preparation: '',
+  },
+  {
+    id: 11,
+    title: '1920s Mystery Detective Chronicles',
+    duration: '9 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-23',
+    preparation: '',
+  },
+  {
+    id: 12,
+    title: 'Medieval Strategic War Campaigns',
+    duration: '10 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-24',
+    preparation: '',
+  },
+  {
+    id: 13,
+    title: 'Underwater VR Exploration Adventure',
+    duration: '11 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-24',
+    preparation: '',
+  },
+  {
+    id: 14,
+    title: '1920s Mystery Detective Chronicles',
+    duration: '9 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-24',
+    preparation: '',
+  },
+  {
+    id: 15,
+    title: 'Medieval Strategic War Campaigns',
+    duration: '10 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-25',
+    preparation: '',
+  },
+  {
+    id: 16,
+    title: 'Underwater VR Exploration Adventure',
+    duration: '11 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-25',
+    preparation: '',
+  },
+  {
+    id: 17,
+    title: '1920s Mystery Detective Chronicles',
+    duration: '9 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-25',
+    preparation: '',
+  },
+  {
+    id: 18,
+    title: 'Medieval Strategic War Campaigns',
+    duration: '10 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-26',
+    preparation: '',
+  },
+  {
+    id: 19,
+    title: 'Underwater VR Exploration Adventure',
+    duration: '11 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-26',
+    preparation: '',
+  },
+  {
+    id: 20,
+    title: '1920s Mystery Detective Chronicles',
+    duration: '9 minutes',
+    ingredients: ['tomate', 'chiles', 'ajo'],
+    schedule_at: '2025-01-26',
+    preparation: '',
+  },
+];
